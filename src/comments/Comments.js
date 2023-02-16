3047b09a5788c80f33b65e0ac81df959d9e1abc0
Kaihan Ren Westin <kaihanren.westin@Kaihans-MBP.lan>
2023-02-16 15:20:53 +0100

    comments updated to key

--- src/comments/Comments.js
+++ src/comments/Comments.js
@@ -1,16 +1,28 @@
 import { useEffect, useState } from "react";
-import { getComments as getCommentsApi} from '../api'
+import { getComments as getCommentsApi } from "../api";
 
-const Comments = ({currentUserId}) => {
-  const [backendComments, setBackendComments] = useState([])
-  console.log("backendComments", backendComments)
+const Comments = ({ currentUserId }) => {
+  const [backendComments, setBackendComments] = useState([]);
+  const rootComments = backendComments.filter(
+    (backendComment) => backendComment.parentId === null
+  );
+  console.log("backendComments", backendComments);
 
-  useEffect(() => {   
-    getCommentsApi().then(data => {
-      setBackendComments(data)
-    }) 
-  }, [])
-  return <div>Comments</div>;
+  useEffect(() => {
+    getCommentsApi().then((data) => {
+      setBackendComments(data);
+    });
+  }, []);
+  return (
+    <div className="comments">
+      <h3 className="comments-title">Comments</h3>
+      <div className="comments-container">
+        {rootComments.map((rootComment) => (
+          <div>{rootComment.body}</div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Comments;
